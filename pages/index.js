4c2a60cb10c3f8fdfa02c855589a062740144500
ErickMarchetti <erickgmarchetti@gmail.com>
2024-12-05 10:10:48 -0300

    dependencies and home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home() {
+  return (
+    <h1>
+      Primeiro deploy do projeto que estou fazendo com o{" "}
+      <a href="https://curso.dev" target="_blank">
+        curso.dev!
+      </a>
+    </h1>
+  )
+}
+
+export default Home
